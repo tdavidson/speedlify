0218a9159959a2646f1fcc3e70bbe3b5cf8d899b
Taylor Davidson <1038836+tdavidson@users.noreply.github.com>
2022-06-07 21:02:37 -0400

    Update and rename sample.js to testing.js

---
+++ _data/sites/testing.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "testing", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://taylordavidson.com",
+		"https://www.helloneighbor.io",
+		"https://foresight.is",
+		"https://unstructuredventures.com"
+	]
+};
