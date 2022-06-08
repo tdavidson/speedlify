b766a29ed22958d0046b48055bf7a58d9846064a
Taylor Davidson <1038836+tdavidson@users.noreply.github.com>
2022-06-07 21:05:09 -0400

    Create taylordavidson.js

---
+++ _data/sites/taylordavidson.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "taylordavidson", // optional, falls back to object key
+	description: "Specific pages on taylordavidson.co",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://taylordavidson.com",
+    "https://taylordavidson.com/hello",
+    "https://taylordavidson.com/invest",
+    "https://taylordavidson.com/all",
+    "https://taylordavidson.com/letters",
+	]
+};
